2678ef94f24e8269967285530e7e9cf1523a0f20
Febin Cherian <febcreationz7@gmail.com>
2024-07-29 19:32:49 +0530

    departed

--- src/components/Passengerdetails.jsx
+++ src/components/Passengerdetails.jsx
@@ -82,7 +82,7 @@ const PassengerDashboard = () => {
                         onClick={() => handleMarkAsDeparted(index)}
                         className="py-1 px-2 bg-red-600 text-white rounded-md hover:bg-red-700"
                       >
-                        Mark as Departed
+                        Departed
                       </button>
                     </td>
                   </tr>
